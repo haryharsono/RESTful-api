e6844f72dbe71b080b766bc4bb053674a694fbb3
haryharsono <311098haryharsono@gmail.com>
2020-10-11 12:58:24 +0800

    setup projek

---
+++ src/main/java/com/haryharsono/JavaRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.haryharsono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestfulApiApplication.class, args);
+	}
+
+}
